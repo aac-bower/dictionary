71b294af34f4e5c599d84cafd85ad8b4611355cf
skuijers <stefan@kuijers.nl>
2016-01-03 13:06:17 +0100

    INIT | tryout bower publish

---
+++ src/app/index.module.js
@@ -0,0 +1,21 @@
+/*
+    module: aac.dictionary
+*/
+
+(function() {
+    'use strict';
+
+    // templates modules to hold all template cache later
+    angular.module('templates', []);
+
+    angular
+        .module(
+            'aac.dictionary', 
+            [
+                'templates',
+
+                
+            ]
+        );
+
+})();
